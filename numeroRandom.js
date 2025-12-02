b45d1188bcfc6e59bf1d8c5bdd7dbbb467ed23db
hector <hector.sanz.7e8@itb.cat>
2025-12-02 20:01:26 +0100

    dg

---
+++ numeroRandom.js
@@ -0,0 +1,27 @@
+let intentos;
+let num;
+
+function piensaRandom() {
+    intentos = 0;
+    num = parseInt(Math.random() * 100);
+    console.log("El número es: " + num)
+}
+
+function comprueba() {
+    let numUser = parseInt(document.getElementById("numUser").value);
+    let msg = ""
+        ; console.log(numUser)
+    if (numUser < num) {
+        msg = "El número es más grande";
+        intentos++;
+    } else {
+        if (numUser > num) {
+            msg = "El número es más pequeño";
+            intentos++;
+        } else {
+            msg = "Has acertado el número: " + num + " en " + intentos + " intentos.";
+        }
+    }
+    console.log(msg);
+    document.getElementById("mensajeUser").innerHTML=msg;
+}
